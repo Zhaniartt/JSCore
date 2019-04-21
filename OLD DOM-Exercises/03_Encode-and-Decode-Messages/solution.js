4f1cf621c26aaa494aee617ab234ca4f7d98cb71
Zhaniartt <46032415+Zhaniartt@users.noreply.github.com>
2019-04-22 00:31:42 +0300

    Update solution.js

--- OLD DOM-Exercises/03_Encode-and-Decode-Messages/solution.js
+++ OLD DOM-Exercises/03_Encode-and-Decode-Messages/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-	//TODO
+	let mainMessage = document.querySelectorAll('#exercise textarea')
+	let buttons = document.querySelectorAll('#exercise button')
+	let encodeButton = buttons[0]
+	let decodeButton = buttons[1]
+	encodeButton.addEventListener('click',encode)
+	decodeButton.addEventListener('click',decode)
+	let currentMssg = ''
+	function encode(){
+		 currentMssg = mainMessage[0].value.split('').map(x=> x.charCodeAt(0))
+										.map(x => x + 1)
+										.map(x=>String.fromCharCode(x))
+										.join("")
+		mainMessage[0].value = ''
+		mainMessage[1].value = currentMssg
+	}
+	function decode(){
+		mainMessage[1].value = mainMessage[1].value.split('').map(x=> x.charCodeAt(0))
+		.map(x => x - 1)
+		.map(x=>String.fromCharCode(x))
+		.join("")
+	}
 }
