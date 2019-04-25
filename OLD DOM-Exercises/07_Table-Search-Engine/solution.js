39840e7ec5dc9fde685a1aa7eb9c9d60f69c9813
Zhaniartt <46032415+Zhaniartt@users.noreply.github.com>
2019-04-25 22:03:34 +0300

    Update solution.js

--- OLD DOM-Exercises/07_Table-Search-Engine/solution.js
+++ OLD DOM-Exercises/07_Table-Search-Engine/solution.js
@@ -1,3 +1,23 @@
-function solve() {
-    //TODO
-}
+function solve () {
+        let rows = Array.from(document.querySelectorAll('tr'))
+        rows.shift()
+        let searchItem = document.querySelector('#searchField')
+        let searchButton = document.querySelector('#searchBtn')
+        searchButton.addEventListener('click', search)
+       
+        function search () {
+          let searchedText = searchItem.value
+       
+          for (let row of rows) {
+            row.removeAttribute('class', 'select')
+       
+            for (let td of Array.from(row.children)) {
+              if (td.textContent.includes(searchedText)) {
+                row.setAttribute('class', 'select')
+                break
+              }
+            }
+          }
+          searchItem.value = ''
+        }
+      }
